a589cf5d583311d0d7d40ca4cca2b7b827303e12
thom_tl <31631330+thomtl@users.noreply.github.com>
2019-08-18 20:34:04 +0200

    Implement mlibc

---
+++ scripts/prepare_initrd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p initrd/usr/{bin,lib}
+
+install -s system-root/usr/lib/ld.so initrd/usr/lib/
+install -s system-root/usr/lib/libc.so initrd/usr/lib/
+install -s system-root/usr/lib/libm.so initrd/usr/lib/
+install -s tools/system-gcc/x86_64-sigma/lib64/libgcc_s.so.1 initrd/usr/lib/
+install -s tools/system-gcc/x86_64-sigma/lib64/libstdc++.so.6 initrd/usr/lib/
+
+install -s ../src/src/sigma-src/initrd/ctest/ctest initrd/usr/bin/
+install -s ../src/src/sigma-src/initrd/test/test initrd/usr/bin/
+
+rm -f ./system-root/usr/sigma/bin/initrd.tar
+
+cd initrd
+tar -czf ../system-root/usr/sigma/bin/initrd.tar * --transform='s/^/\//'
+cd ..
