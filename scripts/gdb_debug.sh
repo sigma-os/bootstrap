aa55a6a2c7589c5cdd160772a7b46ba99f6cd59b
Thomas Woertman <twoertman@gmail.com>
2019-11-10 19:43:33 +0100

    Switch to `sigma.hdd` filename and add license

--- scripts/gdb_debug.sh
+++ scripts/gdb_debug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-qemu-system-x86_64 -hda hdd.bin -enable-kvm -monitor stdio -serial file:/dev/stdout -smp 4 -machine q35 -d int -s -S &
+qemu-system-x86_64 -hda sigma.hdd -enable-kvm -monitor stdio -serial file:/dev/stdout -smp 4 -machine q35 -d int -s -S -cpu host &
 
 gdb ./system-root/usr/sigma/bin/sigma.bin -ix ../src/scripts/gdbinit
--- scripts/run_qemu.sh
+++ scripts/run_qemu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-qemu-system-x86_64 -hda hdd.bin -enable-kvm -monitor stdio -serial file:/dev/stdout -smp 4 -machine q35 -d int -no-shutdown --no-reboot
+qemu-system-x86_64 -enable-kvm -hda sigma.hdd -monitor stdio -serial file:/dev/stdout -smp 4 -machine q35 -no-shutdown --no-reboot -cpu host | c++filt
