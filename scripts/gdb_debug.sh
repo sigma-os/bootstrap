255a561ebad125cf32b5f26d96086ed08d19bff2
Thomas Woertman <twoertman@gmail.com>
2019-11-21 17:22:13 +0100

    Update sigma-kernel build and fix bochs / qemu + gdb

--- scripts/gdb_debug.sh
+++ scripts/gdb_debug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-qemu-system-x86_64 -hda sigma.hdd -enable-kvm -monitor stdio -serial file:/dev/stdout -smp 4 -machine q35 -d int -s -S -cpu host &
+qemu-system-x86_64 -hda sigma.hdd -serial file:/dev/stdout -smp 4 -machine q35 -d int -no-shutdown -no-reboot -s -S &
 
 gdb ./system-root/usr/sigma/bin/sigma.bin -ix ../src/scripts/gdbinit
