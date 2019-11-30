783e343f709bc05cf503c0ee972f8789bd4fee46
Thomas Woertman <twoertman@gmail.com>
2019-11-30 11:26:01 +0100

    Add AHCI driver

--- scripts/prepare_initrd.sh
+++ scripts/prepare_initrd.sh
@@ -11,6 +11,7 @@ install -s tools/system-gcc/x86_64-sigma/lib64/libstdc++.so.6 initrd/usr/lib/
 install -s ../src/src/sigma-src/initrd/ctest/ctest initrd/usr/bin/
 install -s ../src/src/sigma-src/initrd/test/test initrd/usr/bin/
 install -s system-root/usr/bin/zeta initrd/usr/bin/
+install -s system-root/usr/bin/ahci initrd/usr/bin/
 
 rm -f ./system-root/usr/sigma/bin/initrd.tar
 
