c5c55f8419099c024787f1368cdac0b1b6960cd7
Thomas Woertman <twoertman@gmail.com>
2020-01-07 11:28:45 +0100

    Boot from NVMe and make run_qemu better

--- scripts/run_qemu.sh
+++ scripts/run_qemu.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-qemu-system-x86_64 -enable-kvm -hda sigma.hdd -monitor stdio -serial file:/dev/stdout -smp 4 -machine q35 -no-shutdown --no-reboot -cpu host | c++filt
+
+SIGMA_IMAGE="sigma.hdd"
+SIGMA_N_CPUS="4"
+
+QEMU_HDD_AHCI="-hda $SIGMA_IMAGE"
+QEMU_HDD_NVME="-drive file=$SIGMA_IMAGE,if=none,id=nvmedisk0 -device nvme,drive=nvmedisk0,serial=SigmaOSDisk"
+
+QEMU_KVM="-enable-kvm -cpu host"
+QEMU_SMP="-smp $SIGMA_N_CPUS"
+QEMU_MACHINE="-machine q35"
+
+QEMU_DEBUG="-monitor stdio -serial file:/dev/stdout -no-shutdown --no-reboot"
+QEMU_TRACE="-trace nvme_*"
+
+QEMU_ARGS="$QEMU_KVM $QEMU_SMP $QEMU_MACHINE $QEMU_DEBUG $QEMU_TRACE $QEMU_HDD_NVME"
+
+qemu-system-x86_64 $QEMU_ARGS | c++filt
