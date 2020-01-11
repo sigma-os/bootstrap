1c5b4e856761d80f2658ae3f8c96511006efcfed
Thomas Woertman <twoertman@gmail.com>
2020-01-11 13:17:39 +0100

    Disable NVMe trace by default

--- scripts/run_qemu.sh
+++ scripts/run_qemu.sh
@@ -13,6 +13,6 @@ QEMU_MACHINE="-machine q35"
 QEMU_DEBUG="-monitor stdio -serial file:/dev/stdout -no-shutdown --no-reboot"
 QEMU_TRACE="-trace nvme_*"
 
-QEMU_ARGS="$QEMU_KVM $QEMU_SMP $QEMU_MACHINE $QEMU_DEBUG $QEMU_TRACE $QEMU_HDD_NVME"
+QEMU_ARGS="$QEMU_KVM $QEMU_SMP $QEMU_MACHINE $QEMU_DEBUG $QEMU_HDD_NVME"
 
 qemu-system-x86_64 $QEMU_ARGS | c++filt
